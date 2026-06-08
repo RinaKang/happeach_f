16e697b48e76eaa4ff2ca5ba19b0c278fb18f0e6
Yoojin-J <syoojin03@gmail.com>
2026-06-09 01:08:54 +0900

    auth.js console.log 삭제

--- src/features/calendar/hook/auth.js
+++ src/features/calendar/hook/auth.js
@@ -6,7 +6,6 @@ export const getUserIdFromToken = () => {
 
   try {
     const decoded = jwtDecode(token);
-    console.log("디코딩된 전체 payload:", decoded);
     return decoded.sub; // 백엔드가 심어놓은 key 이름
   } catch (error) {
     console.error("토큰 디코딩 실패:", error);
